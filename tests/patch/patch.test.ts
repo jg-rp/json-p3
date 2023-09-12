d24acd6d0c341acfad7db7d0bfd75c86d7709bac
James Prior <jamesgr.prior@gmail.com>
2023-09-12 12:59:51 +0100

    More JSON Patch test coverage

--- tests/patch/patch.test.ts
+++ tests/patch/patch.test.ts
@@ -1,4 +1,4 @@
-import { JSONPatch, JSONPatchError, apply } from "../../src/patch";
+import { JSONPatch, JSONPatchError, OpObject, apply } from "../../src/patch";
 
 describe("JSON Patch", () => {
   test("remove root", () => {
@@ -49,5 +49,97 @@ describe("JSON Patch", () => {
 
     const patch = new JSONPatch(patchObj);
     expect(patch.toArray()).toStrictEqual(patchObj);
+  });
+  test("remove nonexistent array item", () => {
+    const data = { foo: [1, 2, 3] };
+    const patch = new JSONPatch().remove("/foo/99");
+    expect(() => patch.apply(data)).toThrow(JSONPatchError);
+    expect(() => patch.apply(data)).toThrow(
+      "can't remove nonexistent item (remove:0)",
+    );
+  });
+  test("remove nonexistent property", () => {
+    const data = { foo: { bar: [1, 2, 3] } };
+    const patch = new JSONPatch().remove("/foo/baz");
+    expect(() => patch.apply(data)).toThrow(JSONPatchError);
+    expect(() => patch.apply(data)).toThrow(
+      "can't remove nonexistent property (remove:0)",
+    );
+  });
+  test("replace nonexistent array item", () => {
+    const data = { foo: [1, 2, 3] };
+    const patch = new JSONPatch().replace("/foo/99", 42);
+    expect(() => patch.apply(data)).toThrow(JSONPatchError);
+    expect(() => patch.apply(data)).toThrow(
+      "can't replace nonexistent item (replace:0)",
+    );
+  });
+  test("replace nonexistent property", () => {
+    const data = { foo: { bar: [1, 2, 3] } };
+    const patch = new JSONPatch().replace("/foo/baz", 42);
+    expect(() => patch.apply(data)).toThrow(JSONPatchError);
+    expect(() => patch.apply(data)).toThrow(
+      "can't replace nonexistent property (replace:0)",
+    );
+  });
+  test("move, source does no exist", () => {
+    const data = { foo: { bar: [1, 2, 3] } };
+    const patch = new JSONPatch().move("/foo/baz", "/foo/qux");
+    expect(() => patch.apply(data)).toThrow(JSONPatchError);
+    expect(() => patch.apply(data)).toThrow(
+      "source object does not exist (move:0)",
+    );
+  });
+  test("move to root", () => {
+    const data = { foo: { bar: [1, 2, 3] } };
+    const patch = new JSONPatch().move("/foo", "");
+    expect(patch.apply(data)).toStrictEqual({ bar: [1, 2, 3] });
+  });
+  test("copy to root", () => {
+    const data = { foo: { bar: [1, 2, 3] } };
+    const patch = new JSONPatch().copy("/foo", "");
+    expect(patch.apply(data)).toStrictEqual({ bar: [1, 2, 3] });
+    expect(data).toStrictEqual({ foo: { bar: [1, 2, 3] } });
+  });
+  test("constructor, missing op", () => {
+    const opObj: OpObject = { path: "", value: "foo" } as never;
+    expect(() => new JSONPatch([opObj])).toThrow(
+      "expected 'op' to be one of 'add', 'remove', 'replace', 'move', 'copy' or 'test' (undefined:0)",
+    );
+  });
+  test("constructor, missing path", () => {
+    const opObj: OpObject = { op: "add", value: "foo" } as never;
+    expect(() => new JSONPatch([opObj])).toThrow(
+      "missing property 'path' (add:0)",
+    );
+  });
+  test("constructor, missing value", () => {
+    const opObj: OpObject = { op: "add", path: "/foo" } as never;
+    expect(() => new JSONPatch([opObj])).toThrow(
+      "missing property 'value' (add:0)",
+    );
+  });
+  test("constructor, path is not a string", () => {
+    const opObj: OpObject = { op: "add", path: 42, value: "foo" } as never;
+    expect(() => new JSONPatch([opObj])).toThrow(
+      "expected a JSON Pointer string for 'path', found number (add:0)",
+    );
+  });
+  test("constructor, invalid pointer", () => {
+    const opObj: OpObject = { op: "add", path: "bar/", value: "foo" };
+    expect(() => new JSONPatch([opObj])).toThrow(
+      '"bar/" pointers must start with a slash or be the empty string (add:0)',
+    );
+  });
+  test("pointer is not a string", () => {
+    const path: string = 42 as never;
+    expect(() => new JSONPatch().add(path, "foo")).toThrow(
+      "expected a JSON Pointer string, found number (add:0)",
+    );
+  });
+  test("build, invalid pointer", () => {
+    expect(() => new JSONPatch().add("bar/", "foo")).toThrow(
+      '"bar/" pointers must start with a slash or be the empty string (add:0)',
+    );
   });
 });

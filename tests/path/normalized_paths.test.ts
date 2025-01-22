68678603a66db371e52ceeb3faf47b9a4b77cafd
James Prior <jamesgr.prior@gmail.com>
2025-01-22 20:18:08 +0000

    Add placeholder for the normalized path test suite

---
+++ tests/path/normalized_paths.test.ts
@@ -0,0 +1,33 @@
+import { readFileSync } from "fs";
+
+import { JSONPathEnvironment } from "../../src/path/environment";
+import { JSONValue } from "../../src/types";
+
+type Case = {
+  name: string;
+  query: string;
+  document: JSONValue;
+  paths: string[];
+};
+
+const normalized_paths = JSON.parse(
+  readFileSync(
+    process.env.JSONP3_NORMALIZED_PATHS ||
+      "tests/path/normalized_test_suit/normalized_paths.json",
+    {
+      encoding: "utf8",
+    },
+  ),
+);
+
+const env = new JSONPathEnvironment();
+
+describe("compliance normalized paths", () => {
+  test.each<Case>(normalized_paths.tests)(
+    "$name",
+    ({ query, document, paths }: Case) => {
+      const rv = env.query(query, document).paths({ form: "canonical" });
+      expect(paths).toStrictEqual(rv);
+    },
+  );
+});
